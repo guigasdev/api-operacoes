ee1f2693d4ccf1772fc9f1c489eefca530721ddc
Guilherme <pgguilherme337@gmail.com>
2024-10-06 13:32:06 -0300

    feat: subindo arquivos e iniciando repositório

---
+++ src/main/java/com/backend_trabalho/Trabalho/OperacoesApplication.java
@@ -0,0 +1,11 @@
+package com.backend_trabalho.Trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperacoesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OperacoesApplication.class, args);
+	}
+}
